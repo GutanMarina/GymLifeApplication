86b4d8afe7844a3c98e712db107e5c770b678d42
valeriacucoara <valericacucoara@gmail.com>
2025-04-18 11:27:12 +0300

    Add DBModel file with ApplicationDbContext

---
+++ eGymLife.BusinessLogic/DBModel/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eGymLife.Domain.Entities.User;
+using eGymLife.Domain.Entities;
+
+namespace eGymLife.BusinessLogic.DBModel
+{
+    class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() :
+            base("name = eGymLife")
+        {
+        }
+        public DbSet<UDbTable> UDbTable { get; set; }
+
+    }
+
+
+}
+
+
