98bd7426f64c35f3d248e814ea5b0f91e283adf1
valeriacucoara <valericacucoara@gmail.com>
2025-04-18 11:28:17 +0300

    Add Interfaces file with ISession

---
+++ eGymLife.BusinessLogic/Interfaces/ISession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using eGymLife.Domain.Entities.User;
+using eGymLife.Domain.Model.User;
+
+namespace eGymLife.BusinessLogic.Interfaces
+{//declaram metoda
+    public interface ISession
+    {
+        void UserRegister(UDbTable data);
+    }
+}
+
+
