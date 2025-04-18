530856f4e0bbb9eef6dced76cc545fcd47bb0405
valeriacucoara <valericacucoara@gmail.com>
2025-04-18 13:05:55 +0300

    updating BL, Domain, Web

---
+++ eGymLife.BusinessLogic/BusinessLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using eGymLife.BusinessLogic.BlStruct;
+using eGymLife.BusinessLogic.Interfaces;
+
+namespace eGymLife.BusinessLogic
+{//implimentarea la metoda
+    public class BusinessLogic
+    {
+        public ISession GetAuthBl()
+        {
+            return new LoginBl();
+        }
+
+    }
+}
--- eGymLife.Web/Models/Register/UserRegister.cs
+++ eGymLife.Web/Models/Register/UserRegister.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace eGymLife.Web.Models.Register
 {
-	public class UserRegister
-	{
-	}
+    public class UserRegister
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
 }
+
