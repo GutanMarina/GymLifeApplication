9b6ac8b4b7678516ca3c55f044391f293660e5ab
valeriacucoara <valericacucoara@gmail.com>
2025-04-18 11:31:58 +0300

    Updating Login and Register controller

--- eGymLife.Web/Controllers/RegisterController.cs
+++ eGymLife.Web/Controllers/RegisterController.cs
@@ -1,17 +1,62 @@
-﻿using System;
+﻿using eGymLife.BusinessLogic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.SessionState;
+using eGymLife.BusinessLogic.Core;
+using eGymLife.BusinessLogic.Interfaces;
+using eGymLife.BusinessLogic.DBModel;
+using eGymLife.Web.Models.Register;
+using eGymLife.Domain.Enums;
+using eGymLife.Domain.Entities.User;
+using eGymLife.Domain.Model;
+using eGymLife.BusinessLogic.BlStruct;
+
 
 namespace eGymLife.Web.Controllers
 {
     public class RegisterController : Controller
     {
+        private readonly ISession _session;
+        public RegisterController()
+        {
+            var bl = new BusinessLogicBL();
+            _session = bl.GetSessionBL();
+        }
+
+
         // GET: Register
-        public ActionResult Index()
+        public ActionResult Register()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Register(UserRegister model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var userData = new UDbTable
+                {
+                    Username = model.Username,
+                    Email = model.Email,
+                    Password = model.Password,
+                    RegistrationDateTime = DateTime.Now,
+                    RegistrationIp = Request.UserHostAddress,
+                    LastLogin = DateTime.Now, // Inițializare corectă
+                    Level = URole.User // Setare rol implicit
+                };
+
+                _session.UserRegister(userData); // Metoda doar salvează userul
+
+                return RedirectToAction("Register");
+            }
+
+            return View(model);
+        }
+
     }
 }
