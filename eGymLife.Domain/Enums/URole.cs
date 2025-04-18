3a8663327a0d6087f5362f4708ec2c45058c6034
valeriacucoara <valericacucoara@gmail.com>
2025-04-18 11:29:24 +0300

    Add URole

---
+++ eGymLife.Domain/Enums/URole.cs
@@ -0,0 +1,20 @@
+﻿using eGymLife.Domain.Model.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eGymLife.Domain.Enums
+{
+    public enum URole
+    {
+        User = 1,
+        Admin = 2
+
+
+    }
+}
+
+
+
